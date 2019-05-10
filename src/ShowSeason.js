63cebaffc3cba318756a933ae828d447049f691b
ChristopherZev <christopher_zev@icloud.com>
2019-05-10 19:22:37 -0400

    Initiate the app scaffolding to create an app that display the season to the user based on user's location and current time of year, created the ShowSeason comp.

---
+++ src/ShowSeason.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//Created ShowSeason comp. to display seasons based
+//on user location and time of year.
+
+
+const ShowSeason = () => {
+	return(
+		<div>Show me the Season!</div>
+	);
+};
+
+export default ShowSeason;
