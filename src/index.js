6315cd9ddbda6f792862f6cbcac5c8fee6c88423
ChristopherZev <christopher_zev@icloud.com>
2019-05-10 19:38:08 -0400

    Tested a geolocation api based on MDN web docs

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import ShowSeason from './ShowSeason'
 
 const App = () => {
+	window.navigator.geolocation.getCurrentPosition(
+		(position) => console.log(position),
+		(err) => console.log(err)
+	);
 	return(
 		<div> Hello!
 			<ShowSeason/>
