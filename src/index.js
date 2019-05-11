c072d3f36cc76e347c1b6cce375ad0eb6afabce1
ChristopherZev <christopher_zev@icloud.com>
2019-05-10 20:02:30 -0400

    Refactored the App component which was functional to a class based component

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ShowSeason from './ShowSeason'
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		(position) => console.log(position),
-		(err) => console.log(err)
-	);
-	return(
-		<div> Hello!
-			<ShowSeason/>
-		</div>
-		
-	);
-};
+//class App is an exmaple of class based component. The App Class
+//receives functionality/methods from the React.Component via 
+//extends. When making a new class only one method is assigned 
+//to it the render() method. React expects that a class has many
+//methods assigned to it thus needing to extends the 
+//React.Component class(think that you are subclassing)
 
+class App extends React.Component {
+	render() {
+		
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => console.log(position),
+			(err) => console.log(err)
+		);
+		return(
+			<div> Latitude:
+				<ShowSeason/>
+			</div>
+		);
+	}
+}
 
 
 ReactDOM.render(
